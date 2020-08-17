be5b425cc93ed7eb194f064be8414fc881ce1ddd
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-08-17 14:58:23 -0400

    fin

--- src/counter2.js
+++ src/counter2.js
@@ -17,23 +17,24 @@ const History = (props) => {
     )
 }
 
-const Button = ({ onClick, text }) => (
-    <button onClick={onClick}>
-        {text}
+const Display = props => 
+<div>
+    {props.value}
+</div>
+
+const Button = ( props ) => (
+    <button onClick={props.handleClick}>
+        {props.text}
     </button>
 )
 
 
 const Counter2 = props => {
-    const [left, setLeft] = useState(0)
-    const [right, setRight] = useState(0)
-    const [allClicks, setAll] = useState([])
+    const [value, setValue] = useState(10)
 
-
-    const handleLeftClick = () => {
-        setAll(allClicks.concat('L'))
-        setLeft(left + 1)
-    }
+  const setToValue = newValue => {
+    setValue(newValue)
+  }
 
     //  al hacer click en el izq, suma 1 a la izquierda, manteniendo el n° de la derecha
     // const handleLeftClick = () => {
@@ -44,10 +45,6 @@ const Counter2 = props => {
     // }
 
 
-    const handleRightClick = () => {
-        setAll(allClicks.concat('R'))
-        setRight(right + 1)
-    }
     //  al hacer click en el der, suma 1 a la derecha, manteniendo el n° de la izquierda
     // {... clicks} crea un objeto con copias de los valores de los campos de clicks del objeto
     // const handleRightClick = () => {
@@ -59,14 +56,11 @@ const Counter2 = props => {
 
     return (
         <div>
-            <div>
-                {left}
-                <Button onClick={handleLeftClick} text='left' />
-                <Button onClick={handleRightClick} text='right' />
-                {right}
-                <History allClicks={allClicks} />
-            </div>
-        </div>
+        <Display value={value} />
+        <Button handleClick={() => setToValue(1000)} text="thousand" />
+        <Button handleClick={() => setToValue(0)} text="reset" />
+        <Button handleClick={() => setToValue(value + 1)} text="increment" />
+      </div>
     )
 }
 
